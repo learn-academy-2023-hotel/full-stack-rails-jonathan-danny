89231d207c6e58489c2042d6b4b9c91c31858226
jonathanraya <jonathanraya79@gmail.com>
2023-12-04 17:44:30 -0600

    story 3 complete

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get '/blogs' => 'blog#index', as: 'blogs'
+  get 'blogs/new' => 'blog#new', as: 'new_blog'
   get '/blogs/:id' => 'blog#show' , as: 'blog'
   root 'blog#index'
 end
