8290290c480e9f5cb4897632cc6fbff58cc92cc6
Danny De La Rosa <danny.devlarosa@gmail.com>
2023-12-04 16:30:59 -0800

    create story compltete"

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   get '/blogs' => 'blog#index', as: 'blogs'
   get 'blogs/new' => 'blog#new', as: 'new_blog'
   get '/blogs/:id' => 'blog#show' , as: 'blog'
+  post 'blogs' => 'blog#create'
   root 'blog#index'
 end
